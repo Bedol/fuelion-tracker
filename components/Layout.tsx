2dc9b8a858c3b81a4a1274d94a27686f24ea64e8
Bed0l <bartlomiej.weber@gmail.com>
2021-03-29 11:32:46 +0200

    Revert "Using primereact MenuBar as menu"
    This reverts commit 300d0fde1bc08ad6794556765a2253a0f858d203.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navigation />
-      <div className="container mx-auto py-4">
+      <div className="container mx-auto">
         <main>{children}</main>
       </div>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome to Fuelion Tracker</h1>
-      <p>Track your money speded to the fuel</p>
+      <div className="md:container md:mx-auto py-4">
+        <h1>Welcome to Fuelion Tracker</h1>
+        <p>Track your money speded to the fuel</p>
+      </div>
     </div>
   );
 }
