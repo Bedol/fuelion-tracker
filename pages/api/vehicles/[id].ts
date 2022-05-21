6e286eb4a3b02a5c861f1111effcd4bef9ee7a8f
Bed0l <bartlomiej.weber@gmail.com>
2022-05-21 20:31:47 +0200

    refactor: update get vehicle api request

--- pages/api/vehicles/[id].ts
+++ pages/api/vehicles/[id].ts
@@ -1,23 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import {
-  FuelType,
-  Gearbox,
-  MilageType,
-  PowerUnit,
-} from "../../../types/vehicle_types";
 
-export default ({ query: { id } }: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({
-    id: id,
-    type: "passenger",
-    brand: "Nissan",
-    model: "Quashqai 2",
-    production_year: 2019,
-    engine_capacity: 1.3,
-    fuel_type: FuelType.Gasoline,
-    gearbox: Gearbox.Automatic,
-    power: 130,
-    power_unit: PowerUnit.HorsePower,
-    milage_in: MilageType.Kilometers,
+export default async (
+  { query: { id } }: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const vehicle = await prisma.vehicles.findUnique({
+    where: { id: id.toString() },
   });
+
+  res.json(vehicle);
 };
