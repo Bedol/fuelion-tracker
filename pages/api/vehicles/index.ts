a5f10a9e5e19bcef85e97f1f0f429939b2f83973
Bed0l <bartlomiej.weber@gmail.com>
2022-05-21 19:46:02 +0200

    fix: api prisma query

--- pages/api/vehicles/index.ts
+++ pages/api/vehicles/index.ts
@@ -1,10 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, PrismaPromise, Vehicles } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-export default async (_req: NextApiRequest, resp: NextApiResponse) => {
-  const allVehicles = prisma.vehicles.findMany();
+export default async (
+  _req: NextApiRequest,
+  resp: NextApiResponse<PrismaPromise<Vehicles[]> | {}>
+) => {
+  const allVehicles = await prisma.vehicles.findMany();
 
   resp.json(allVehicles);
 };
