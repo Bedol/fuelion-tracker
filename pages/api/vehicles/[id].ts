b95656c0bfe86ea88d2d7008dde976af14f1d09a
Bed0l <bartlomiej.weber@gmail.com>
2022-11-28 19:09:25 +0100

    fix: fix build project

--- pages/api/vehicles/[id].ts
+++ pages/api/vehicles/[id].ts
@@ -1,10 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../lib/prisma";
 
-const handle = async (
-  { query: { id } }: NextApiRequest,
-  res: NextApiResponse
-) => {
+const handle = async (req: NextApiRequest, res: NextApiResponse) => {
+  const id: number = req.body.id;
   const vehicle = await prisma.vehicles.findUnique({
     where: { id: id },
   });
