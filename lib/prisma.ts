03ba4f53998cbbb779e703dd01fae2249f452238
Bed0l <bartlomiej.weber@gmail.com>
2022-11-28 18:35:39 +0100

    refactor: create global prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
+let prisma: PrismaClient;
 
-declare global {
-  // eslint-disable-next-line no-unused-vars
-  var prisma: PrismaClient | undefined;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+export default prisma;
