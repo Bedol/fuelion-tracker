6c5b268bb416fbb104821be259abd26fe5b7a305
Bed0l <bartlomiej.weber@gmail.com>
2022-11-28 22:27:54 +0100

    style: basic fueling form

--- pages/fueling/new.tsx
+++ pages/fueling/new.tsx
@@ -2,19 +2,20 @@ import { useRouter } from "next/router";
 import React from "react";
 
 const NewFueling = () => {
-  const [amount, setAmount] = React.useState(0);
-  const [cost, setCost] = React.useState(0);
+  const [formData, setFormData] = React.useState({
+    amount: 0,
+    cost: 0,
+  });
   const router = useRouter();
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
-    const body = { amount, cost };
 
     try {
       await fetch("/api/fueling", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
+        body: JSON.stringify(formData),
       });
 
       await router.push("/fueling/new");
@@ -24,11 +25,11 @@ const NewFueling = () => {
   };
 
   return (
-    <div>
-      <h2>Add your fuel cost</h2>
+    <div className="px-2 py-4">
+      <h2 className="mb-6">Add your fuel cost</h2>
 
-      <form>
-        {/* data */}
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        {/* date */}
         {/* country */}
         {/* region */}
         {/* station */}
@@ -41,27 +42,50 @@ const NewFueling = () => {
         {/* tire type */}
         {/* air conditioning */}
 
-        <label htmlFor="amount">Amount of fuel</label>
-        <input
-          type="number"
-          name="amount"
-          id="amount"
-          onChange={(e) => {
-            setAmount(e.target.valueAsNumber);
-          }}
-          value={amount}
-        />
+        <div className="mb-4">
+          <label
+            htmlFor="amount"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Amount of fuel
+          </label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={(e) =>
+              setFormData({ ...formData, amount: e.target.valueAsNumber })
+            }
+            value={formData.amount}
+          />
+        </div>
 
-        <label htmlFor="cost">Cost of fuel</label>
-        <input
-          type="number"
-          name="cost"
-          id="cost"
-          onChange={(e) => setCost(e.target.valueAsNumber)}
-          value={cost}
-        />
+        <div className="mb-4">
+          <label
+            htmlFor="cost"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Cost of fuel
+          </label>
+          <input
+            type="number"
+            name="cost"
+            id="cost"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={(e) =>
+              setFormData({ ...formData, cost: e.target.valueAsNumber })
+            }
+            value={formData.cost}
+          />
+        </div>
 
-        <input type="submit" value="Add" onClick={handleSubmit} />
+        <input
+          type="submit"
+          value="Add"
+          className="input-submit bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          onClick={handleSubmit}
+        />
       </form>
     </div>
   );
