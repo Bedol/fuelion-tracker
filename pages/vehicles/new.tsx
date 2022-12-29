6201e81f29c8a08f2057d3da9cef7c23e3069136
Bed0l <bartlomiej.weber@gmail.com>
2022-12-29 22:18:35 +0100

    style: new vehicle form - WIP

--- pages/vehicles/new.tsx
+++ pages/vehicles/new.tsx
@@ -1,8 +1,48 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+
 const NewVehicle = () => {
   return (
-    <div>
-      <h1>Add New Vehicle</h1>
-    </div>
+    <Box>
+      <Text fontSize="2xl" mb="2">
+        Add New Vehicle
+      </Text>
+      <Flex flexDir="column" justifyContent="space-between">
+        <Box as="form">
+          <FormControl>
+            <FormLabel htmlFor="brand">Brand</FormLabel>
+            <Input type="text" name="brand" id="brand" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="model">Model</FormLabel>
+            <Input type="text" name="model" id="model" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="production_year">Production Year</FormLabel>
+            <Input type="number" name="production_year" id="production_year" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="engine_capacity">Engine Capacity</FormLabel>
+            <Input type="number" name="engine_capacity" id="engine_capacity" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="mileage_in">Mileage In</FormLabel>
+            <Input type="number" name="mileage_in" id="mileage_in" />
+          </FormControl>
+
+          <Button colorScheme="telegram" variant="outline" my="4">
+            Save
+          </Button>
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
