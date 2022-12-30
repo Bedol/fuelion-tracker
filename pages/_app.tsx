627c80a5310f031707ea2e576c845a91557d444a
Bed0l <bartlomiej.weber@gmail.com>
2022-12-30 13:06:47 +0100

    style: prettier fix code style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Layout from "../components/Layout";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 
 const queryClient = new QueryClient();
