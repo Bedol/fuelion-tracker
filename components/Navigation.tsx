d29ef546fb8ba8c38227bfb7e28047bf720edc88
Bed0l <bartlomiej.weber@gmail.com>
2022-12-30 12:56:22 +0100

    refactor: remove unused links from navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import {
   Box,
   Button,
@@ -7,34 +6,33 @@ import {
   Flex,
   HStack,
   useColorModeValue,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
+import Link from "next/link";
 
 const Navigation = () => {
   return (
     <Box as="section" pb="12">
-      <Box as="nav" bg="bg-surface" boxShadow={useColorModeValue('sm', 'sm-dark')}>
+      <Box
+        as="nav"
+        bg="bg-surface"
+        boxShadow={useColorModeValue("sm", "sm-dark")}
+      >
         <Container py="4">
           <HStack spacing="10" justify="space-between">
             <Flex justify="space-between" flex="1">
               <ButtonGroup variant="link" spacing="8">
-                <Link href="/fueling/new" passHref>
-                  <Button as="a" colorScheme="teal" variant="ghost">Add Refueling</Button>
-                </Link>
-                <Link href="/expenses/new" passHref>
-                  <Button as="a" colorScheme="teal" variant="ghost">Add Expenses</Button>
-                </Link>
                 <Link href="/vehicles" passHref>
-                  <Button as="a" colorScheme="teal" variant="ghost">Your Vehicles</Button>
-                </Link>
-                <Link href="/profiles" passHref>
-                  <Button as="a" colorScheme="teal" variant="ghost">Profiles</Button>
+                  <Button as="a" colorScheme="teal" variant="ghost">
+                    Your Vehicles
+                  </Button>
                 </Link>
               </ButtonGroup>
             </Flex>
           </HStack>
         </Container>
       </Box>
-    </Box>)
+    </Box>
+  );
 };
 
 export default Navigation;
