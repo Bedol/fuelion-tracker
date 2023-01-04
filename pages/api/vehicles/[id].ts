096c251e5b5b8cced3edb72be2b46eee9c2e703a
Bed0l <bartlomiej.weber@gmail.com>
2023-01-04 20:04:33 +0100

    refactor: update api for vehicles

--- pages/api/vehicles/[id].ts
+++ pages/api/vehicles/[id].ts
@@ -4,11 +4,38 @@ import { prisma } from "../../../lib/prisma";
 
 const handle = async (req: NextApiRequest, res: NextApiResponse<Vehicles>) => {
   const { id } = req.query;
-  const vehicle = await prisma.vehicles.findUnique({
-    where: { id: parseInt(id as string) },
-  });
+  const { method, body } = req;
 
-  res.json(vehicle);
+  switch (method) {
+    case "GET":
+      {
+        const result = await prisma.vehicles.findUnique({
+          where: { id: parseInt(id as string) },
+        });
+        res.json(result);
+      }
+      break;
+    case "PUT":
+      {
+        const result = await prisma.vehicles.update({
+          where: { id: parseInt(id as string) },
+          data: body,
+        });
+        res.json(result);
+      }
+      break;
+    case "DELETE":
+      {
+        const result = await prisma.vehicles.delete({
+          where: { id: parseInt(id as string) },
+        });
+        res.json(result);
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["PUT", "DELETE", "GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
 };
 
 export default handle;
