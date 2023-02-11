bcc242f2ad153634fa803e73a2b32c6c6482813b
Bed0l <bartlomiej.weber@gmail.com>
2023-02-11 15:47:04 +0100

    style: reformat code after update prettier config

--- pages/api/fueling/index.ts
+++ pages/api/fueling/index.ts
@@ -1,48 +1,48 @@
-import { Fueling, Prisma } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next/types";
-import prisma from "../../../lib/prisma";
+import { Fueling, Prisma } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next/types';
+import prisma from '../../../lib/prisma';
 
 export default async function handle(
-  req: NextApiRequest,
-  res: NextApiResponse<Prisma.PrismaPromise<Fueling[]> | {}>
+	req: NextApiRequest,
+	res: NextApiResponse<Prisma.PrismaPromise<Fueling[]> | {}>
 ) {
-  const { method, body } = req;
+	const { method, body } = req;
 
-  switch (method) {
-    case "POST":
-      {
-        const fueling = await prisma.fueling.create({
-          data: body,
-        });
+	switch (method) {
+		case 'POST':
+			{
+				const fueling = await prisma.fueling.create({
+					data: body,
+				});
 
-        // Find vehicle and update mileage if needed
-        const vehicle = await prisma.vehicles.findUnique({
-          where: {
-            id: body.vehicleId,
-          },
-        });
+				// Find vehicle and update mileage if needed
+				const vehicle = await prisma.vehicles.findUnique({
+					where: {
+						id: body.vehicleId,
+					},
+				});
 
-        if (vehicle.mileage < body.mileage) {
-          await prisma.vehicles.update({
-            where: {
-              id: body.vehicleId,
-            },
-            data: {
-              mileage: body.mileage,
-            },
-          });
-        }
-        res.json(fueling);
-      }
-      break;
-    case "GET":
-      {
-        const fueling = await prisma.fueling.findMany();
-        res.json(fueling);
-      }
-      break;
-    default:
-      res.setHeader("Allow", ["POST", "GET"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
-  }
+				if (vehicle.mileage < body.mileage) {
+					await prisma.vehicles.update({
+						where: {
+							id: body.vehicleId,
+						},
+						data: {
+							mileage: body.mileage,
+						},
+					});
+				}
+				res.json(fueling);
+			}
+			break;
+		case 'GET':
+			{
+				const fueling = await prisma.fueling.findMany();
+				res.json(fueling);
+			}
+			break;
+		default:
+			res.setHeader('Allow', ['POST', 'GET']);
+			res.status(405).end(`Method ${method} Not Allowed`);
+	}
 }
