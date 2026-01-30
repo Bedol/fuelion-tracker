d4f34a6e89da06b8c3cd76a5d734d70e8acdef77
Bartlomiej Weber <bartlomiej.weber@gmail.com>
2026-01-31 00:00:51 +0100

    feat(01-01): integrate LocaleProvider into app
    - Import LocaleProvider from contexts/LocaleContext
    - Wrap LocaleProvider around Layout component
    - Provider hierarchy: SessionProvider > LocaleProvider > Layout
    - LocaleProvider inside ChakraProvider for Chakra component access
    - All components now have access to useLocale hook and t() function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
-import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+	HydrationBoundary,
+	QueryClient,
+	QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { LocaleProvider } from '../contexts/LocaleContext';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
@@ -15,11 +20,13 @@ function MyApp({ Component, pageProps }) {
 		<ChakraProvider value={system}>
 			<QueryClientProvider client={queryClient}>
 				<SessionProvider session={pageProps.session}>
-					<Layout>
-						<HydrationBoundary state={pageProps.dehydratedState}>
-							<Component {...pageProps} />
-						</HydrationBoundary>
-					</Layout>
+					<LocaleProvider>
+						<Layout>
+							<HydrationBoundary state={pageProps.dehydratedState}>
+								<Component {...pageProps} />
+							</HydrationBoundary>
+						</Layout>
+					</LocaleProvider>
 				</SessionProvider>
 				{!isProduction && <ReactQueryDevtools initialIsOpen={false} />}
 			</QueryClientProvider>
